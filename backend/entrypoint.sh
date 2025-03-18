bd19f6101a431fe8d0f5c3bcc6a06cbbbbc9879c
KevinValesSalas <kevin.g.valles@gmail.com>
2025-03-18 14:04:30 +1300

    Adjusting entrypoint file to run import completion rates component

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+echo "Importing initial data..."
+python manage.py import_completion_rates
+
 echo "Starting Gunicorn..."
 exec gunicorn collection_log_backend.wsgi:application --bind 0.0.0.0:$PORT
